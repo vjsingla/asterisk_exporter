67a9413fdd028fee98562ab542b027a4262cab1a
Vikramjeet Singla <vikramjeet.singla@gmail.com>
2020-12-14 15:10:45 +0700

    Added main file

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"os/exec"
+	"strconv"
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			aChannels, err := executeCommand("asterisk -rx 'core show channels' | grep 'active channels' | awk '{print $1}'")
+			if err != nil {
+				fmt.Println(err)
+			}
+			activeChannels.Set(aChannels)
+
+			aCalls, err := executeCommand("asterisk -rx 'core show channels' | grep 'active calls' | awk '{print $1}'")
+			if err != nil {
+				fmt.Println(err)
+			}
+			activeCalls.Set(aCalls)
+
+			cProcessed, err := executeCommand("asterisk -rx 'core show channels' | grep 'calls processed' | awk '{print $1}'")
+			if err != nil {
+				fmt.Println(err)
+			}
+			callProcessed.Set(cProcessed)
+		}
+	}()
+}
+
+func executeCommand(cmdStr string) (float64, error) {
+	var o float64
+	cmd := exec.Command("sh", "-c", cmdStr)
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		return o, err
+	}
+
+	o, err = strconv.ParseFloat(string(stdoutStderr), 8)
+	if err != nil {
+		return o, err
+	}
+
+	return o, nil
+
+}
+
+var (
+	activeChannels = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "asterisk_active_channels",
+		Help: "Total current active channels",
+		ConstLabels: map[string]string{
+			"type": "active channels",
+		},
+	})
+	activeCalls = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "asterisk_active_calls",
+		Help: "Total current active calls",
+		ConstLabels: map[string]string{
+			"type": "active calls",
+		},
+	})
+	callProcessed = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "asterisk_calls_processed",
+		Help: "Total current calls processed",
+		ConstLabels: map[string]string{
+			"type": "calls processed",
+		},
+	})
+)
+
+func main() {
+	recordMetrics()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":9200", nil)
+}
